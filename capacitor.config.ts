11279f485ef12e1e42e08359c9dd4aa6383daee7
CristianDani8989 <guzmandani503@gmail.com>
2026-01-14 12:31:51 -0500

    1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MUSICCG_UPCA',
+  webDir: 'www'
+};
+
+export default config;
